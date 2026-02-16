ae96c839f9e6ec6ecd03470db34bb1078ca79fef
RommualD <rommualdesrosiers@gmail.com>
2026-02-15 20:37:12 -0500

    this is the final code for this this working code

---
+++ Lab_04_01_SalesTax.java
@@ -0,0 +1,11 @@
+public class Lab_04_01_SalesTax{
+    public static void main(String[] args){
+
+        double purchasePrice = 80.00;
+        double taxRate = 0.05;
+        double salesTax = purchasePrice * taxRate;
+
+        System.out.println("The purchase price is: " + purchasePrice);
+        System.out.println("The sales tax at 5% is: " + salesTax);
+    }
+}
